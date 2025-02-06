9bb92799ff3149ec366da3616020bf63b70cd735
Makechi <makechieric9@gmail.com>
2025-02-07 02:25:31 +0300

    Update the logo file name

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const Logo = () => {
     return (
         <Image
-            src={`/assets/images/invisio-logo-without-bg.webp`}
-            alt={`Invisio logo`}
+            src={`/assets/images/finviq-logo-without-bg.webp`}
+            alt={`Finviq logo`}
             width={768}
             height={768}
             className={`w-16 aspect-square`}
